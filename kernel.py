6f8f3ca727113b10be40c55b8bf40157089452bf
SatxaFortuny <satxafp@gmail.com>
2026-04-24 00:13:38 +0200

    changed from node-focused to edge-focused

--- kernel.py
+++ kernel.py
@@ -5,14 +5,13 @@ import triton.language as tl
 def mpnn_forward(
     node_feat_ptr, stride_nf_n,    # node features
     coord_ptr, stride_coord_n,     # x y z coordinates
-    row_ptr,                       # index array
-    col_ptr,                       # source node
-    edge_ptr, stride_edge_n,       # edge features
+    src_idx_ptr,                   # source array
+    dst_idx_ptr,                   # destiny array
+    edge_feat_ptr, stride_edge_n,       # edge features
     
     # Weights
     w_msg_ptr,                       # Message MLP weights
     w_mov_ptr,                       # Movement MLP weights
-    w_nod_ptr,                       # Node MLP weights
     
     # RBF
     rbf_centers_ptr,
@@ -22,77 +21,69 @@ def mpnn_forward(
     out_feat_ptr, stride_ofeat_n,
     out_coord_ptr, stride_ocoord_n,
     
-    num_nodes,
+    num_edges,
     
-    BLOCK_N: tl.constexpr,         # number of nodes x process
-    BLOCK_NEIGHBORS: tl.constexpr, # max neighbors x node
+    BLOCK_E: tl.constexpr,         # number of edges x process
     F_NODE: tl.constexpr,          # num features x node
     F_EDGE: tl.constexpr,          # num features x edge
     RBF_DIM: tl.constexpr,         # number of gauss bells  
+    HIDDEN_FEATURES: tl.constexpr,
+    OUT_FEATURES: tl.constexpr
 ):
 
     # 1. Identification
     process_index = tl.program_id(axis=0)
-    node_index = process_index * BLOCK_N + tl.arange(0, BLOCK_N)
+    edge_index = process_index * BLOCK_E + tl.arange(0, BLOCK_E)
     
-    # 2. Features, coordinates and csr load
-    x_nod_base_addr = node_feat_ptr + process_index * BLOCK_N * stride_nf_n
-    c_nod_base_addr = coord_ptr + process_index * BLOCK_N * stride_coord_n
-    row_addr = row_ptr + node_index
-    source_addr = row_ptr + node_index + 1
-
-    vertical_sequence = tl.arange(0, BLOCK_N)
-    horizontal_x_sequence = tl.arange(0, F_NODE)
-    horizontal_c_sequence = tl.arange(0, 3)
-
-    x_nod_addr = x_nod_base_addr + (vertical_sequence[:, None] * stride_nf_n) + (horizontal_x_sequence[None, :])
-    c_nod_addr = c_nod_base_addr + (vertical_sequence[:, None] * stride_coord_n) + (horizontal_c_sequence[None, :])
+    # 2. COO node fetch
+    node1_addr = src_idx_ptr + edge_index
+    node2_addr = dst_idx_ptr + edge_index
     
-    # the mask compares the indexes of the tensor, not the contents
-    x_nod_mask = (node_index[:, None] < num_nodes) & (horizontal_x_sequence[None, :] < F_NODE)
-    c_nod_mask = (node_index[:, None] < num_nodes) & (horizontal_c_sequence[None, :] < 3)
-    csr_mask = (node_index < num_nodes)
-
-    X_node = tl.load(x_nod_addr, mask=x_nod_mask, other=0.0)
-    C_node = tl.load(c_nod_addr, mask=c_nod_mask, other=0.0)
-    row = tl.load(row_addr, mask=csr_mask, other=0)
-    source = tl.load(source_addr, mask=csr_mask, other=0)
+    edges_mask = (edge_index < num_edges)
     
-    # 3. Neighbors fetch
-    neighbor_sequence = tl.arange(0, BLOCK_NEIGHBORS)
-    neighbors_indexes = row[:, None] + neighbor_sequence[None, :]
-    neighbors_addrs = col_ptr + neighbors_indexes
-    neighbor_mask = (node_index[:, None] < num_nodes) & (neighbors_indexes < source[:, None])
-    neighbors = tl.load(neighbors_addrs, mask=neighbor_mask, other=0)
-
-    # 4. Neighbors features and coordinates load
-    n_feat_addr = node_feat_ptr + (neighbors[:, :, None] * stride_nf_n) + horizontal_x_sequence[None, None, :]
-    n_coord_addr = coord_ptr + (neighbors[:, :, None] * stride_coord_n) + horizontal_c_sequence[None, None, :]
-
-    n_feat_mask = neighbor_mask[:, :, None] & (horizontal_x_sequence[None, None, :] < F_NODE)
-    n_coord_mask = neighbor_mask[:, :, None] & (horizontal_c_sequence[None, None, :] < 3)
-
-    n_features = tl.load(neighbors_feat_addr, mask=n_feat_mask, other=0.0)
-    n_coordinates = tl.load(neighbors_coord_addr, mask=n_coord_mask, other=0.0)
-
-    # 5. Distance calculator
+    node1 = tl.load(node1_addr, mask=edges_mask, other=0)
+    node2 = tl.load(node2_addr, mask=edges_mask, other=0)
+    
+    # 3. Node and edges features and coordinates fetch
+    n_feat_sequence = tl.arange(0, F_NODE)
+    edge_sequence = tl.arange(0, F_EDGE)
+    coord_sequence = tl.arange(0, 4)
+    
+    n1_feat_addr = node_feat_ptr + (node1[:, None] * stride_nf_n) + n_feat_sequence[None, :]
+    n2_feat_addr = node_feat_ptr + (node2[:, None] * stride_nf_n) + n_feat_sequence[None, :]
+    e_feat_addr = edge_feat_ptr + (edge_index[:, None] * stride_edge_n) + edge_sequence[None, :]
+    n1_coord_addr = coord_ptr + (node1[:, None] * stride_coord_n) + coord_sequence[None, :]
+    n2_coord_addr = coord_ptr + (node2[:, None] * stride_coord_n) + coord_sequence[None, :]
+    
+    node_mask = edges_mask[:, None] & (n_feat_sequence[None, :] < F_NODE)
+    e_feat_mask = edges_mask[:, None] & (edge_sequence[None, :] < F_EDGE)
+    coord_mask = edges_mask[:, None] & (coord_sequence[None, :] < 3)
+    
+    n1_feat = tl.load(n1_feat_addr, mask=node_mask, other=0.0)
+    n2_feat = tl.load(n2_feat_addr, mask=node_mask, other=0.0)
+    e_feat = tl.load(e_feat_addr, mask=e_feat_mask, other=0.0)
+    n1_coord = tl.load(n1_coord_addr, mask=coord_mask, other=0.0)
+    n2_coord = tl.load(n2_coord_addr, mask=coord_mask,  other=0.0)
+    
+    # 4. RBF distances calculation
     rbf_seq = tl.arange(0, RBF_DIM)
     centers = tl.load(rbf_centers_ptr + rbf_seq)
 
-    x_inc = n_coordinates - C_node[:, None, :]
+    x_inc = n1_coord - n2_coord
     x_inc = x_inc*x_inc
-    x = tl.sum(x_inc, axis=2)
+    x = tl.sum(x_inc, axis=1)
     x = tl.sqrt(x)
-    centered_x = x[:, :, None] - centers[None, None, :]
+    centered_x = x[:, None] - centers[None, :]
     distances = tl.exp(-rbf_gamma * (centered_x * centered_x))
-
-    # 6. Message obtention
-    horizontal_e_sequence = tl.arange(0, F_EDGE)
-    e_features_addr = edge_ptr + ()
     
-    # 7. Movement obtention
+    # 5. MLP partial message obtention
+    w_m_sequence = tl.arange(0, OUT_FEATURES)
+    n2_sequence = F_NODE + n_feat_sequence
     
-    # 8. Final message calculation
+    w_m_node_feat_addr = w_msg_ptr + (n_feat_sequence[:, None] * HIDDEN_FEATURES) + w_m_sequence[None, :]
+    W_node_message = tl.load(w_m_node_feat_addr, other=0.0)
     
-    # 9. New features obtention and storing
+    # 6. MLP edges movement obtention
+    
+    # 7. Messages and distances saving
     
